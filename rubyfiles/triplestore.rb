9978648399f41abbd2abb7ab11e6583eb50d9f7a
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-15 18:16:50 +0100

    triplestore.rb - be more idomatic Ruby
    Use def, rather than lambdas
    
    Use heredocs.
    
    Use puts instead of system("echo ...")
    
    Use IO.write(file, content) instead of system("echo #{content} > #{file}")

--- rubyfiles/triplestore.rb
+++ rubyfiles/triplestore.rb
@@ -12,36 +12,50 @@ ssh = "ssh"
 pass = SeOpenData::Utils::PasswordStore.new(use_env_vars: $config_map["USE_ENV_PASSWORDS"])
 Log.debug "Checking ENV for passwords" if pass.use_env_vars?
 
+def get_rdfxml(x,y)
+  puts "Creating #{y} from #{x}"
+  system "curl --silent -H 'Accept: application/rdf+xml' -L '#{x}' > '#{y}'"
+end
+
+def get_rdfxml_for_virtuoso(x,y)
+  get_rdfxml x, $config_map["GEN_VIRTUOSO_DIR"]+y
+end
+
 begin
   if !File.file?($config_map["VIRTUOSO_SCRIPT_LOCAL"])
     Log.debug "Creating #{$config_map["VIRTUOSO_SCRIPT_LOCAL"]}"
+   
+    get_rdfxml_for_virtuoso(
+      $config_map["ESSGLOBAL_URI"]+"vocab/",
+      "essglobal_vocab.rdf"
+    )
+    get_rdfxml_for_virtuoso(
+      $config_map["ESSGLOBAL_URI"]+"standard/organisational-structure",
+      "organisational-structure.skos"
+    )
     
-    get_rdfxml_curl = 'curl --silent -H "Accept: application/rdf+xml" -L'
-    get_rdfxml = -> (x,y) {system("echo 'Creating #{y} from #{x}' && #{get_rdfxml_curl} #{x} > #{y}")}
-    get_rdfxml_for_virtuoso = -> (x,y) {get_rdfxml.call(x,$config_map["GEN_VIRTUOSO_DIR"]+y)}
+    puts "Creating #{$config_map["VIRTUOSO_NAMED_GRAPH_FILE"]}"
+    IO.write $config_map["VIRTUOSO_NAMED_GRAPH_FILE"], $config_map["GRAPH_NAME"]
 
-    get_rdfxml_for_virtuoso.call($config_map["ESSGLOBAL_URI"]+"vocab/","essglobal_vocab.rdf")
-    get_rdfxml_for_virtuoso.call($config_map["ESSGLOBAL_URI"]+"standard/organisational-structure","organisational-structure.skos")
-
-    system("echo 'Creating #{$config_map["VIRTUOSO_NAMED_GRAPH_FILE"]}'")
-    system("echo '#{$config_map["GRAPH_NAME"]}' > #{$config_map["VIRTUOSO_NAMED_GRAPH_FILE"]}")
-
-    system("echo 'Creating #{$config_map["VIRTUOSO_SCRIPT_LOCAL"]}'")
-    system("echo \"SPARQL CLEAR GRAPH '#{$config_map["GRAPH_NAME"]}';\" > #{$config_map["VIRTUOSO_SCRIPT_LOCAL"]} ")
-    system("echo \"ld_dir('#{$config_map["VIRTUOSO_DATA_DIR"]}','*.rdf',NULL);\" >> #{$config_map["VIRTUOSO_SCRIPT_LOCAL"]}")
-    system("echo \"ld_dir('#{$config_map["VIRTUOSO_DATA_DIR"]}','*.skos',NULL);\" >> #{$config_map["VIRTUOSO_SCRIPT_LOCAL"]}")
-    system("echo \"rdf_loader_run();\" >> #{$config_map["VIRTUOSO_SCRIPT_LOCAL"]}")
-
-
-    system("echo Transfering directory '#{$config_map["GEN_VIRTUOSO_DIR"]}' to virtuoso server '#{$config_map["DEPLOYMENT_SERVER"]}':#{$config_map["VIRTUOSO_DATA_DIR"]}")
-    system("#{ssh} #{$config_map["DEPLOYMENT_SERVER"]} 'mkdir -p #{$config_map["VIRTUOSO_DATA_DIR"]}'")
-    system("#{rsync} #{$config_map["GEN_VIRTUOSO_DIR"]} #{$config_map["DEPLOYMENT_SERVER"]}:#{$config_map["VIRTUOSO_DATA_DIR"]}")
-
+    puts "Creating #{$config_map["VIRTUOSO_SCRIPT_LOCAL"]}"
+    IO.write $config_map["VIRTUOSO_SCRIPT_LOCAL"], <<HERE
+SPARQL CLEAR GRAPH '#{$config_map["GRAPH_NAME"]}';
+ld_dir('#{$config_map["VIRTUOSO_DATA_DIR"]}','*.rdf',NULL);
+ld_dir('#{$config_map["VIRTUOSO_DATA_DIR"]}','*.skos',NULL);
+rdf_loader_run();
+HERE
+    
+    puts "Transfering directory '#{$config_map["GEN_VIRTUOSO_DIR"]}' to virtuoso server '#{$config_map["DEPLOYMENT_SERVER"]}':#{$config_map["VIRTUOSO_DATA_DIR"]}"
+                                                  
+    system <<HERE
+#{ssh} #{$config_map["DEPLOYMENT_SERVER"]} 'mkdir -p #{$config_map["VIRTUOSO_DATA_DIR"]}' &&
+#{rsync} #{$config_map["GEN_VIRTUOSO_DIR"]} #{$config_map["DEPLOYMENT_SERVER"]}:#{$config_map["VIRTUOSO_DATA_DIR"]}
+HERE
 
     if($config_map["AUTO_LOAD_TRIPLETS"])
-      puts "ssh -T #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba ******** #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'"
+      puts "#{ssh} -T #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba ******** #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'"
       pass = pass.get $config_map["VIRTUOSO_PASS_FILE"]
-      system("ssh #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba #{pass} #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'")
+      system("#{ssh} #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba #{pass} #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'")
     else
       puts <<HERE
 ****
