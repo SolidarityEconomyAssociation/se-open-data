7b629d5bcfaf88db2b8da7bbce3b0541021ca188
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-15 15:46:09 +0100

    se_open_data/rubyfiles/triplestore.rb - get Virtuoso password from `pass`
    Via the PasswordStore module. Allow environment passwords.

--- rubyfiles/triplestore.rb
+++ rubyfiles/triplestore.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 require "./load_config"
-
+require_relative "../lib/load_path"
+require "se_open_data/utils/password_store"
 
 rsync = "rsync -avz"
 ssh = "ssh"
 
+# This gets (encrypted) passwords. Read the documentation in the class.
+pass = SeOpenData::Utils::PasswordStore.new(use_env_vars: $config_map["USE_ENV_PASSWORDS"])
 
 if !File.file?($config_map["VIRTUOSO_SCRIPT_LOCAL"])
 
@@ -31,14 +34,15 @@ if !File.file?($config_map["VIRTUOSO_SCRIPT_LOCAL"])
 
 
     if($config_map["AUTO_LOAD_TRIPLETS"])
-        puts "ssh #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba #{$config_map["VIRTUOSO_PASS"]} #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'"
-        system("ssh #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba #{$config_map["VIRTUOSO_PASS"]} #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'")
+        puts "ssh -T #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba ******** #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'"
+        pass = pass.get $config_map["VIRTUOSO_PASS_FILE"]
+        system("ssh #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba #{pass} #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'")
     else
         system("echo \"****\"")
         system("echo \"**** IMPORTANT! ****\"")
         system("echo \"**** The final step is to load the data into Virtuoso with graph named #{$config_map["GRAPH_NAME"]}:\"")
         system("echo \"**** Execute the following command, providing the password for the Virtuoso dba user:\"")
-        system("echo \"****\tssh #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba #{$config_map["VIRTUOSO_PASS"]} #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'\"")
+        system("echo \"****\tssh #{$config_map["DEPLOYMENT_SERVER"]} 'isql-vt localhost dba ******** #{$config_map["VIRTUOSO_SCRIPT_REMOTE"]}'\"")
     end    
 else
     puts "File exists: #{$config_map['VIRTUOSO_SCRIPT_LOCAL']}"
