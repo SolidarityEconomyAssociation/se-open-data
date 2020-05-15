36f973b82322e6a32a811410f4ac9168cc386187
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-15 17:47:45 +0100

    se_open_data/utils/log_factory.rb - convenient factory for loggers
    I want to be able to trace-log things simply at various levels,
    without too much boiler plate code or explicit config required. This
    is an attempt to do that.

---
+++ lib/se_open_data/utils/log_factory.rb
@@ -0,0 +1,95 @@
+require 'logger'
+
+
+module SeOpenData
+  module Utils
+
+    # Defines a configurable logging mechanism
+    #
+    # Designed to be simple to include, without needing to be
+    # configured every time. Instead, it defines what we hope are
+    # sensible defaults and optional configuration are permitted, to
+    # fine tune these.
+    #
+    # Usage example, in a file foo/bar.rb:
+    #
+    #     require 'se_open_data/utils/log_factory'
+    #
+    #     Log = SeOpenData::Utils::LogFactory.default
+    #     
+    #     # ... in code somewhere later
+    #        Log.debug "something happened"
+    #
+    # This will print something like this to STDERR:
+    #
+    #     D, [2020-05-15T17:17:19.830963 #12604] DEBUG -- foo/bar.rb: something happened
+    #
+    # See {default} for more details.
+    class LogFactory
+
+      # This gets a default logger instance, creating it if necessary,
+      # observing externally defined configuration.  The {default}
+      # factory method knows which file created it and names it
+      # accordingly.
+      #
+      # Configuration can optionally be supplied via environment
+      # variables with names prefixed with `SEA_LOG_`. Specifically:
+      #
+      # `SEA_LOG_LEVEL`: defines the log level. Should be one of:
+      # - `debug`
+      # - `info`
+      # - `warn`
+      # - `error`
+      # - `fatal`
+      # - `unknown`
+      #
+      # (Defaults to `warn`).
+      #
+      # `SEA_LOG_FILE`: should be the path to a file, relative to the
+      # current working directory. (Default is to log to STDERR.)
+      #
+      # `SEA_LOG_CONTEXT`: specifies what to form of the file context
+      # use to label loggers with. Specifically, can be:
+      #
+      # - `filename`: only the filename is used
+      # - `rel`: the relative path is used
+      # - `abs`: the absolute path is used
+      # - `none`: no context label is printed
+      #
+      # (Defaults to `short`.)
+      #
+      # @return a new Logger instance
+      def self.default
+        
+        # Set the progname
+        caller_label =
+          case ENV['SEA_LOG_CALLER']
+          when 'filename'
+            File.basename(caller_locations.first.path)
+          when 'abs'
+            File.absolute_path(caller_locations.first.path)
+          when 'rel'
+            caller_locations.first.path
+          when 'none'
+            nil
+          else
+            caller_locations.first.path
+          end
+
+        # Create the logger
+        logger ||= Logger.new(
+          ENV['SEA_LOG_FILE'] || STDERR,
+          level: ENV['SEA_LOG_LEVEL'] || Logger::WARN,
+        )
+
+        logger.progname = caller_label if caller_label
+
+        logger.debug("Created logger in #{caller_locations.first.path}")
+        
+        return logger
+      end
+      
+    end
+  end
+end
+
