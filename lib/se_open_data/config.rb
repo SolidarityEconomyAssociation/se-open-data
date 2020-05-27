1546c72802810ed5d502a77629a859f1b521d6bb
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-27 21:13:59 +0100

    ica-youth-network/test/load_config.rb -> tools/se_open_data/lib/se_open_data/config.rb
    Move tests too, and adjust code slightly to pass tests within test
    suite. (Requires minor changes to avoid warnings which weren't
    apparent before.)

---
+++ lib/se_open_data/config.rb
@@ -0,0 +1,201 @@
+module SeOpenData
+  # Reads a simple key-value plain-text config file.
+  #
+  # Values are delimited by an `=` character. Expected values are
+  # expanded with some hard-wired know-how, and some directories are
+  # found relative to the {base_dir} parameter, which defaults to the
+  # caller script's directory.
+  #
+  # This is an abstraction layer from the config file itself.
+  # i.e. Variables here are independant from names in the config file.
+  class Config
+    require 'fileutils'
+    
+    # @param file [String] - the path to the config file to load.
+    # @param base_dir [String] - the base directory in which to locate certain paths
+    def initialize(file, base_dir = Config.caller_dir)
+      @config_file = file
+
+      @map = {}
+      File.foreach(@config_file).with_index(1) do |line, num|
+        next if line =~ /^\s*$/ # skip blank lines
+        next if line =~ /^\s*#/ # skip comments
+
+        # Split on the first =, trim the resulting string pair.
+        # If no =, val will be nil.
+        key, val = line.split("=", 2).map(&:strip)
+
+        # Guard against invalid key characters. This is almost certainly a mistake
+        raise "invalid config key '#{key}' at line #{num}" unless valid_key? key
+        
+        # Guard against no '='. Likewise a mistake.
+        raise "config line with no '=' delimiter on line #{num}" if val.nil?
+        
+        # Guard against duplicates. Likewise a mistake.
+        raise "config key '#{key}' duplicated on line #{num}" if @map.has_key? key
+
+        # Add the key and value
+        @map[key] = val
+      end
+
+      # setup Config
+      # setup lib path, this needs to be changed
+
+      # csv.rb
+      def join(*args)  # joins using local path delimiter
+        File.join(*args)
+      end
+      def unixjoin(first, *rest) # uses the unix '/' delimiter
+        #First part must have trailing slash removed only, rest must
+        # have (a single) leading slash.
+        first.gsub(%r{/+$},'')+rest.map {|it| it.gsub(%r{^/*},"/") }.join
+      end
+      
+      # Expand these paths relative to base_dir
+      %w(TOP_OUTPUT_DIR SRC_CSV_DIR CSS_SRC_DIR SE_OPEN_DATA_LIB_DIR SE_OPEN_DATA_BIN_DIR)
+        .each do |key| # expand rel to base_dir, append a slash
+          @map[key] = join File.expand_path(@map[key], base_dir), ""
+        end
+
+      # This is the directory where we generate intermediate csv files
+      @map["GEN_CSV_DIR"] = join @map["TOP_OUTPUT_DIR"], "csv", ""
+
+      #goal end file (standard.csv)
+      @map["STANDARD_CSV"] = join @map["TOP_OUTPUT_DIR"], @map["STANDARD_CSV"]
+      #csv.rb end
+      
+      #generate.rb
+      @map["WWW_DIR"] = unixjoin @map["TOP_OUTPUT_DIR"], "www", ""
+      @map["GEN_DOC_DIR"] = unixjoin @map["WWW_DIR"], "doc", ""
+      @map["GEN_CSS_DIR"] = unixjoin @map["GEN_DOC_DIR"], "css", ""
+      @map["GEN_VIRTUOSO_DIR"] = unixjoin @map["TOP_OUTPUT_DIR"], "virtuoso", ""
+      @map["GEN_SPARQL_DIR"] = unixjoin @map["TOP_OUTPUT_DIR"], "sparql", ""
+      @map["SPARQL_GET_ALL_FILE"] = unixjoin @map["GEN_SPARQL_DIR"], "query.rq"
+      @map["SPARQL_LIST_GRAPHS_FILE"] = unixjoin @map["GEN_SPARQL_DIR"], "list-graphs.rq"
+      @map["SPARQL_ENDPOINT_FILE"] = unixjoin @map["GEN_SPARQL_DIR"], "endpoint.txt"
+      @map["SPARQL_GRAPH_NAME_FILE"] = unixjoin @map["GEN_SPARQL_DIR"], "default-graph-uri.txt"
+      @map["DATASET_URI_BASE"] = "#{@map["URI_SCHEME"]}://#{@map["URI_HOST"]}/#{@map["URI_PATH_PREFIX"]}"
+      @map["GRAPH_NAME"] = @map["DATASET_URI_BASE"]
+      @map["ONE_BIG_FILE_BASENAME"] = unixjoin @map["GEN_VIRTUOSO_DIR"], "all"
+      
+      @map["CSS_FILES"] =  Dir[join @map["CSS_SRC_DIR"], "*.css"].join(",")
+      @map["SAME_AS_FILE"] = @map.key?("SAMEAS_CSV") ? @map["SAMEAS_CSV"] : "" 
+      @map["SAME_AS_HEADERS"] = @map.key?("SAMEAS_HEADERS") ? @map["SAMEAS_HEADERS"] : "" 
+
+      #generate.rb
+
+      #deploy.rb
+      @map["DEPLOYMENT_DOC_SUBDIR"] = @map["URI_PATH_PREFIX"]
+      @map["DEPLOYMENT_DOC_DIR"] = unixjoin @map["DEPLOYMENT_WEBROOT"], @map["DEPLOYMENT_DOC_SUBDIR"]
+
+      #deploy.rb
+
+      #triplestore.rb
+      @map["VIRTUOSO_NAMED_GRAPH_FILE"] = unixjoin @map["GEN_VIRTUOSO_DIR"], "global.graph"
+      @map["VIRTUOSO_SQL_SCRIPT"] = "loaddata.sql"
+
+      @map["VERSION"] = make_version
+      @map["VIRTUOSO_DATA_DIR"] = unixjoin @map["VIRTUOSO_ROOT_DATA_DIR"], @map["VERSION"], ""
+      @map["VIRTUOSO_SCRIPT_LOCAL"] = join @map["GEN_VIRTUOSO_DIR"], @map["VIRTUOSO_SQL_SCRIPT"]
+      @map["VIRTUOSO_SCRIPT_REMOTE"] = unixjoin @map["VIRTUOSO_DATA_DIR"], @map["VIRTUOSO_SQL_SCRIPT"]
+
+      #triplestore.rb
+
+      #create_w3id.rb
+      @map["W3ID_LOCAL_DIR"] = join @map["TOP_OUTPUT_DIR"], "w3id", ""
+      @map["HTACCESS"] = join @map["W3ID_LOCAL_DIR"], ".htaccess"
+      @map["W3ID_REMOTE_SSH"] = "#{@map["DEPLOYMENT_SERVER"]}:#{@map["W3ID_REMOTE_LOCATION"]}#{@map["URI_PATH_PREFIX"]}"
+      @map["REDIRECT_W3ID_TO"] = "#{@map["URI_SCHEME"]}://#{@map["SERVER_ALIAS"]}/#{@map["URI_PATH_PREFIX"]}"
+      #create_w3id.rb
+
+      # Preserve booleans in these cases
+      %w(AUTO_LOAD_TRIPLETS USE_ENV_PASSWORDS).each do |key|
+        @map[key] = @map.key?(key) && @map[key].to_s.downcase == "true"
+      end
+
+      #end config
+
+      # Make sure these dirs exist
+      FileUtils.mkdir_p @map.fetch_values(
+        'GEN_CSV_DIR',
+        'GEN_CSS_DIR',
+        'GEN_VIRTUOSO_DIR',
+        'GEN_SPARQL_DIR',
+        'W3ID_LOCAL_DIR'
+      )
+    end
+
+    # Checks whether key is valid
+    #
+    # Valid keys must contain only alphanumeric characters, hyphens or underscores.
+    # @return [Boolean] true if it is valid.
+    def valid_key?(key)
+      key !~ /[^-\w]/i
+    end
+
+    # A convenient method for #map.fetch
+    #
+    # @param args (See Hash#fetch)
+    # @return (See Hash#fetch)
+    def fetch(*args)
+      @map.fetch(*args)
+    end
+
+    # A convenient method for #map.has_key?
+    #
+    # @param args (See Hash#has_key?)
+    # @return (See Hash#has_key?)
+    def has_key?(key)
+      @map.has_key? key
+    end
+
+    # Gets the underlying config hash
+    def map
+      @map
+    end
+    
+    #f stands for file
+    def gen_ruby_command(in_f, script, options, out_f, err_f)
+      #generate ruby commands to execute ruby scripts for pipelined processes
+      rb_template = "ruby -I " + @map["SE_OPEN_DATA_LIB_DIR"]
+
+      command = ""
+
+      command += "#{rb_template} #{script}"
+      if options
+        command += " #{options}"
+      end
+
+      if in_f
+        command += " #{in_f}"
+      end
+
+      if out_f
+        command += " > #{out_f}"
+      end
+      if out_f && err_f
+        command += " > #{err_f}"
+      end
+
+      puts command
+      system(command)
+    end
+
+
+    protected
+
+    # For overriding in tests
+    def make_version
+      t = Time.now
+      "#{t.year}#{t.month}#{t.day}#{t.hour}#{t.min}#{t.sec}"
+    end
+    
+    private
+
+    # Used only in the constructor as a default value for base_dir
+    def self.caller_dir
+      File.dirname(caller_locations(2, 1).first.absolute_path)
+    end
+    
+  end
+end
---
+++ test/config/test_config.rb
@@ -0,0 +1,179 @@
+require_relative "../../lib/load_path"
+require "se_open_data/config"
+require "minitest/autorun"
+require "fileutils"
+
+Minitest::Test::make_my_diffs_pretty!
+
+# Stubbed subclass for testing
+class TestConfig < SeOpenData::Config
+  def make_version # prevent this changing second to second
+    "2020526214656"
+  end
+end
+
+describe SeOpenData::Config do
+  caller_dir = File.absolute_path(__dir__)
+  generated_dir = caller_dir+"/generated-data"
+  
+
+  describe "a valid config instance" do
+    config_map = nil
+    
+    lib_dir = File.absolute_path(__dir__+ "/../../tools/se_open_data")
+
+    # TestConfig should recreate this + some contents
+    FileUtils.rm_r generated_dir if File.exist? generated_dir
+
+    # expansions relative to caller_dir, i.e. this script's dir
+    config = TestConfig.new(caller_dir+"/config/valid.txt")
+
+    # Make recursive directory listing here, before other tests delete it
+    listing = Dir.glob(generated_dir+"/**/*", File::FNM_DOTMATCH)
+                .map {|it| it.delete_prefix(generated_dir)+"\n" }
+                .sort
+                .join
+    
+    config_map = config.map
+    
+    #puts config_map
+    expected_map = {
+      "MULTI_EQUAL" => "foo=bar=baz",
+      "EMPTY_EQUAL" => "",
+      "with-Hyphen-Lowercase0123456789" => "# this is not a comment #",
+      "WITH_PADDING" => "with padding",
+      "USE_ENV_PASSWORDS" => false,
+      "SRC_CSV_DIR" => caller_dir+"/original-data/",
+      "ORIGINAL_CSV_1" => "Youth-ledCoops.csv",
+      "URI_SCHEME" => "https",
+      "URI_HOST" => "w3id.solidarityeconomy.coop",
+      "URI_PATH_PREFIX" => "ica-youth-network/",
+      "DEPLOYMENT_SERVER" => "sea-0-admin",
+      "DEPLOYMENT_WEBROOT" => "/var/www/html/data1.solidarityeconomy.coop/",
+      "DEPLOYMENT_RSYNC_FLAGS" => "--delete",
+      "ESSGLOBAL_URI" => "https://w3id.solidarityeconomy.coop/essglobal/V2a/",
+      "VIRTUOSO_ROOT_DATA_DIR" => "/home/admin/Virtuoso/BulkLoading/Data/",
+      "SPARQL_ENDPOINT" => "http://store1.solidarityeconomy.coop:8890/sparql",
+      "TOP_OUTPUT_DIR" => caller_dir+"/generated-data/",
+      "STANDARD_CSV" => caller_dir+"/generated-data/standard.csv",
+      "AUTO_LOAD_TRIPLETS" => true,
+      "SE_OPEN_DATA_BIN_DIR" => lib_dir+"/bin/",
+      "SE_OPEN_DATA_LIB_DIR" => lib_dir+"/lib/",
+      "CSS_SRC_DIR" => caller_dir+"/css/",
+      "VIRTUOSO_PASS_FILE" => "deployments/dev-0.solidarityeconomy.coop/virtuoso/dba.password",
+      "W3ID_REMOTE_LOCATION" => "/var/www/html/w3id.org/",
+      "SERVER_ALIAS" => "data1.solidarityeconomy.coop",
+      "TEST_INITIATIVE_IDENTIFIERS" => "16 40",
+      "GEN_CSV_DIR" => caller_dir+"/generated-data/csv/",
+      "WWW_DIR" => caller_dir+"/generated-data/www/",
+      "GEN_DOC_DIR" => caller_dir+"/generated-data/www/doc/",
+      "GEN_CSS_DIR" => caller_dir+"/generated-data/www/doc/css/",
+      "GEN_VIRTUOSO_DIR" => caller_dir+"/generated-data/virtuoso/",
+      "GEN_SPARQL_DIR" => caller_dir+"/generated-data/sparql/",
+      "SPARQL_GET_ALL_FILE" => caller_dir+"/generated-data/sparql/query.rq",
+      "SPARQL_LIST_GRAPHS_FILE" => caller_dir+"/generated-data/sparql/list-graphs.rq",
+      "SPARQL_ENDPOINT_FILE" => caller_dir+"/generated-data/sparql/endpoint.txt",
+      "SPARQL_GRAPH_NAME_FILE" => caller_dir+"/generated-data/sparql/default-graph-uri.txt",
+      "DATASET_URI_BASE" => "https://w3id.solidarityeconomy.coop/ica-youth-network/",
+      "GRAPH_NAME" => "https://w3id.solidarityeconomy.coop/ica-youth-network/",
+      "ONE_BIG_FILE_BASENAME" => caller_dir+"/generated-data/virtuoso/all",
+      "CSS_FILES" => caller_dir+"/css/2.css,"+caller_dir+"/css/1.css",
+      "SAME_AS_FILE" => "",
+      "SAME_AS_HEADERS" => "",
+      "DEPLOYMENT_DOC_SUBDIR" => "ica-youth-network/",
+      "DEPLOYMENT_DOC_DIR" => "/var/www/html/data1.solidarityeconomy.coop/ica-youth-network/",
+      "VIRTUOSO_NAMED_GRAPH_FILE" => caller_dir+"/generated-data/virtuoso/global.graph",
+      "VIRTUOSO_SQL_SCRIPT" => "loaddata.sql",
+      "VERSION" => "2020526214656",
+      "VIRTUOSO_DATA_DIR" => "/home/admin/Virtuoso/BulkLoading/Data/2020526214656/",
+      "VIRTUOSO_SCRIPT_LOCAL" => caller_dir+"/generated-data/virtuoso/loaddata.sql",
+      "VIRTUOSO_SCRIPT_REMOTE" => "/home/admin/Virtuoso/BulkLoading/Data/2020526214656/loaddata.sql",
+      "W3ID_LOCAL_DIR" => caller_dir+"/generated-data/w3id/",
+      "HTACCESS" => caller_dir+"/generated-data/w3id/.htaccess",
+      "W3ID_REMOTE_SSH" => "sea-0-admin:/var/www/html/w3id.org/ica-youth-network/",
+      "REDIRECT_W3ID_TO" => "https://data1.solidarityeconomy.coop/ica-youth-network/"
+    }
+    
+    it "should generate an expected map" do
+      value(config_map).must_equal expected_map
+    end
+
+    expected_listing = <<-HERE
+/.
+/csv
+/csv/.
+/sparql
+/sparql/.
+/virtuoso
+/virtuoso/.
+/w3id
+/w3id/.
+/www
+/www/.
+/www/doc
+/www/doc/.
+/www/doc/css
+/www/doc/css/.
+HERE
+    
+    it "should create the expected directories" do
+      value(listing).must_equal expected_listing
+    end
+  end
+
+  describe "a config instance with duplicates" do
+
+    # TestConfig should recreate this + some contents
+    FileUtils.rm_r generated_dir if File.exist? generated_dir
+
+    it "should raise an exception" do
+      err = proc do
+        TestConfig.new(caller_dir+"/config/invalid-dupes.txt")
+      end
+              .must_raise RuntimeError
+      
+      err.message.must_match "config key 'SOMETHING' duplicated on line 3"
+    end
+  end
+
+  describe "a config instance with a invalid keys" do
+
+    # TestConfig should recreate this + some contents
+    FileUtils.rm_r generated_dir if File.exist? generated_dir
+
+    it "(space) should raise an exception" do
+      err = proc do
+        TestConfig.new(caller_dir+"/config/invalid-keys-1.txt")
+      end
+              .must_raise RuntimeError
+      
+      err.message.must_match "invalid config key 'SOMETHING ELSE' at line 2"
+    end
+    
+    it "(colon) should raise an exception" do
+      err = proc do
+        TestConfig.new(caller_dir+"/config/invalid-keys-2.txt")
+      end
+              .must_raise RuntimeError
+      
+      err.message.must_match "invalid config key 'SOMETHING:ELSE' at line 2"
+    end
+  end
+  
+  describe "a config instance with missing delimiters" do
+
+    # TestConfig should recreate this + some contents
+    FileUtils.rm_r generated_dir if File.exist? generated_dir
+
+    it "should raise an exception" do
+      err = proc do
+        TestConfig.new(caller_dir+"/config/invalid-delims.txt")
+      end
+              .must_raise RuntimeError
+      
+      err.message.must_match "config line with no '=' delimiter on line 2"
+    end
+  end
+  # FIXME test missing mandatory values
+  # also unknown values?
+end
