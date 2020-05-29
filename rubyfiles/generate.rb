0f4b48c23719cac0863e6b531cb920e7570119f4
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-29 20:19:46 +0100

    tools/se_open_data/rubyfiles/generate.rb - refactor to use SeOpenData::Config

--- rubyfiles/generate.rb
+++ rubyfiles/generate.rb
@@ -1,28 +1,32 @@
 #!/usr/bin/env ruby
-require "./load_config"
+require_relative "../lib/load_path"
+require "se_open_data/config"
 
-if !File.file?($config_map["ONE_BIG_FILE_BASENAME"])
-  system("rsync -r #{$config_map["CSS_SRC_DIR"]} #{$config_map["GEN_CSS_DIR"]}")
+config_file = Dir.glob('settings/{config,defaults}.txt').first 
+config = SeOpenData::Config.new 'settings/config.txt', Dir.pwd
+
+if !File.file?(config.ONE_BIG_FILE_BASENAME)
+  system("rsync -r #{config.CSS_SRC_DIR} #{config.GEN_CSS_DIR}")
 
 
   #all
-  system("echo '#{$config_map["SPARQL_ENDPOINT"]}' > #{$config_map["SPARQL_ENDPOINT_FILE"]}")
-  system("echo '#{$config_map["GRAPH_NAME"]}' > #{$config_map["SPARQL_GRAPH_NAME_FILE"]}")
+  system("echo '#{config.SPARQL_ENDPOINT}' > #{config.SPARQL_ENDPOINT_FILE}")
+  system("echo '#{config.GRAPH_NAME}' > #{config.SPARQL_GRAPH_NAME_FILE}")
 
-  csv_to_rdf = $config_map["SE_OPEN_DATA_BIN_DIR"]+"csv/standard/csv-to-rdf.rb"
+  csv_to_rdf = config.SE_OPEN_DATA_BIN_DIR+"csv/standard/csv-to-rdf.rb"
 
-  options = ["--output-directory #{$config_map["GEN_DOC_DIR"]} ",
-  "--uri-prefix #{$config_map["DATASET_URI_BASE"]} ",
-  "--essglobal-uri #{$config_map["ESSGLOBAL_URI"]} ",
-  "--one-big-file-basename #{$config_map["ONE_BIG_FILE_BASENAME"]} ",
-  "--sameas-csv '#{$config_map["SAME_AS_FILE"]}' ",
-  "--sameas-headers '#{$config_map["SAME_AS_HEADERS"]}' ",
-  "--map-app-sparql-query-filename #{$config_map["SPARQL_GET_ALL_FILE"]} ",
-  "--css-files '#{$config_map["CSS_FILES"]}'"].join("\\\n")
+  options = ["--output-directory #{config.GEN_DOC_DIR} ",
+  "--uri-prefix #{config.DATASET_URI_BASE} ",
+  "--essglobal-uri #{config.ESSGLOBAL_URI} ",
+  "--one-big-file-basename #{config.ONE_BIG_FILE_BASENAME} ",
+  "--sameas-csv '#{config.SAME_AS_FILE}' ",
+  "--sameas-headers '#{config.SAME_AS_HEADERS}' ",
+  "--map-app-sparql-query-filename #{config.SPARQL_GET_ALL_FILE} ",
+  "--css-files '#{config.CSS_FILES}'"].join("\\\n")
 
-  in_file = $config_map["STANDARD_CSV"]
+  in_file = config.STANDARD_CSV
 
-  Config.gen_ruby_command(in_file,csv_to_rdf,options,nil,nil)
+  config.gen_ruby_command(in_file,csv_to_rdf,options,nil,nil)
 else
   puts "Work done already"
 end
