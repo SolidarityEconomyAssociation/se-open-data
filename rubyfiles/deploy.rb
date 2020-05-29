d9133d60cee8a7f3ecb69359c48879e5c2d81a39
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-05-29 20:25:31 +0100

    tools/se_open_data/rubyfiles/deploy.rb - refactor to use SeOpenData::Config

--- rubyfiles/deploy.rb
+++ rubyfiles/deploy.rb
@@ -1,14 +1,20 @@
 #!/usr/bin/env ruby
-require "./load_config"
+require_relative "../lib/load_path"
+require "se_open_data/config"
+
+# this just does an rsync, after creating the target directory
+
+config_file = Dir.glob('settings/{config,defaults}.txt').first 
+config = SeOpenData::Config.new 'settings/config.txt', Dir.pwd
 
 rsync = "rsync -avz --no-perms --omit-dir-times"
 ssh = "ssh"
 
-ssh_mkdir_cmd = "#{ssh} #{$config_map["DEPLOYMENT_SERVER"]} "+
-    "'cd #{$config_map["DEPLOYMENT_WEBROOT"]} && mkdir -p #{$config_map["DEPLOYMENT_DOC_SUBDIR"]}'" 
+ssh_mkdir_cmd = "#{ssh} #{config.DEPLOYMENT_SERVER} "+
+    "'cd #{config.DEPLOYMENT_WEBROOT} && mkdir -p #{config.DEPLOYMENT_DOC_SUBDIR}'" 
 
-rsync_cmd = "#{rsync} #{$config_map["DEPLOYMENT_RSYNC_FLAGS"]} "+
-    "#{$config_map["GEN_DOC_DIR"]} #{$config_map["DEPLOYMENT_SERVER"]}:#{$config_map["DEPLOYMENT_DOC_DIR"]}"
+rsync_cmd = "#{rsync} #{config.DEPLOYMENT_RSYNC_FLAGS} "+
+    "#{config.GEN_DOC_DIR} #{config.DEPLOYMENT_SERVER}:#{config.DEPLOYMENT_DOC_DIR}"
 
 
 #fail early
