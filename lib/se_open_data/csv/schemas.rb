9129ead215b635551fdc99fcb728f28502779a76
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-03-02 21:41:05 +0000

    se_open_data/csv/schemas.rb - define fields more precisely, where possible.
    Descriptions included, and some thought about how to interpret the
    namespace of the identifiers.
    
    Essentially, we are stealing the 'prefix' and 'base namespace' ideas
    from TTL, but assuming that these are defined external to this
    schema (and can be dependent on the dataset).

--- lib/se_open_data/csv/schemas.rb
+++ lib/se_open_data/csv/schemas.rb
@@ -742,6 +742,25 @@ HERE
           version: 4,
           description: <<-HERE,
 
+## Terms or identifiers
+
+Where a field is defined as a term from a SKOS vocabulary or other
+identifier, the namespace varies with the dataset. Strictly this
+should be explicitly defined, but is outside the scope of this schema.
+Full linked-data URIs can be used, which should be globally unique, or
+URIs with prefixes denoted by some abbreviation, delimited by a colon,
+or identifiers in some base namespace, as commonly used in TTL. If the
+latter, the base namespace and prefixes should be defined elsewhere
+such that the full globally-unique URI can be inferred.
+
+This means that identifiers used prior to the tighening-up of this
+schema definition are mostly still valid if we define the base
+namespace appropriately. The difficulty is when multiple namespaces
+are allowed - such as mixing country identifiers with ICA regions, if
+they require different prefixes to be inferred. But this still works
+if an over-arching vocabulary is defined, which can then be declared
+the base namespace. A small tweak would be to declare prefixes.
+
 ## Geographic coordinates
 
 `Latitude` and `Longitude` are for the exact geolocation of the SSE
@@ -765,10 +784,12 @@ Sometimes a single column can take values that are in fact a list.  So
 we need to know the character used to separate the items in the list.
 For example, in the legal_form column, we might have an initiative
 that is both a 'Cooperative' and a 'Company', the cell would then have
-the value "Cooperative;Company"
+the value "Cooperative;Company". This of course assumes that the
+delimited identifiers do not contain any semi-colons themselves. There
+is no current provision for cases where this is not true.
 
 HERE
-          comment: 'Added "jurisdiction" field, redefined "country_name" as "country_id',
+          comment: 'Added "territory_id" field, redefined "country_name" as "country_id',
           primary_key: [:id],
           fields: [
             {id: :id,
@@ -784,56 +805,79 @@ HERE
             },
             {id: :name,
              header: 'Name',
-             desc: '',
-             comment: '',
+             desc: 'The name of the initiative, as UTF-8 plain text',
+             comment: 'Typically in English, but this depends on the context',
             },
             {id: :description,
              header: 'Description',
-             desc: '',
-             comment: '',
+             desc: 'A description of the initiative, as a UTF-8 encoded HTML fragment',
+             comment: <<-HERE,
+Typically in English, but this depends on the context. Hopefully not with malicious content
+which could compromise security if embedded in a webpage (such as phishing links or 
+scripts), but this should validated and not be assumed blindly to be safe.
+HERE
             },
             {id: :organisational_structure,
              header: 'Organisational Structure',
-             desc: '',
+             desc: <<-HERE,
+The initiative's category of organisational structure. Allowed values are 
+single term URIs from a relevant vocabulary.
+HERE
              comment: <<-HERE,
-This was known as legal_form. The allowed values are taken from the
-relevant ESSGLOBAL SKOS vocabulary.
+This was known as legal_form. 
 HERE
             },
             {id: :primary_activity,
              header: 'Primary Activity',
-             desc: '',
+             desc: <<-HERE,
+The main category of actitivity executed by the initiative. Allowed values are 
+single terms from a relevant vocabulary.
+HERE
              comment: '',
             },
             {id: :activities,
              header: 'Activities',
-             desc: '',
+             desc: <<-HERE,
+Any additional categories of actitivity executed by the initiative. Allowed values are 
+zero or more terms from a relevant vocabulary, separated by semi-colons.
+HERE
              comment: '',
             },
             {id: :street_address,
              header: 'Street Address',
-             desc: '',
+             desc: <<-HERE,
+An optional street name and postbox number (where applicable) for the main address of
+the initiative.
+HERE
              comment: '',
             },
             {id: :locality,
              header: 'Locality',
-             desc: '',
+             desc: <<-HERE,
+An optional sub-regional locality (e.g. town, city, parish) for the main address of the initiative.
+Typically a textual name, in the local language, UTF-8 encoded.
+HERE
              comment: '',
             },
             {id: :region,
              header: 'Region',
-             desc: '',
+             desc: <<-HERE,
+An optional sub-national region for the main address of the initiative.
+Typically a textual name, in the local language, UTF-8 encoded.
+HERE
              comment: '',
             },
             {id: :postcode,
              header: 'Postcode',
-             desc: '',
+             desc: <<-HERE,
+An optional postcode or zipcode, applicable in the context of the main address.
+HERE
              comment: '',
             },
             {id: :country_id,
              header: 'Country ID',
              desc: <<-HERE,
-This indicates the country in which the main address is located.
+An optional country identifier for the main address.
 Should be an uppercase ISO-3166-1 two-letter country code.
 HERE
              comment: '',
@@ -842,74 +886,102 @@ HERE
              header: 'Territory ID',
              desc: <<-HERE ,
 Indicates the territory in which the initiative operates. Values
-should be an ISO-3166-1 two-letter country code, or one of the
-ICA's region/super-region identifiers. Upper case where applicable.
+should be a term from a relevant vocabulary, or other identifier.
 HERE
-             comment: 'There are ESSGLOBAL SKOS vocabs for the ISO and ICA identifiers',
+             comment: <<-HERE,
+There are ESSGLOBAL SKOS vocabs for the ISO and ICA region/super-region identifiers
+HERE
             },
             {id: :homepage,
              header: 'Website',
-             desc: '',
+             desc: "An optional valid URL for the initiative's main web page",
              comment: '',
             },
             {id: :phone,
              header: 'Phone',
-             desc: '',
-             comment: '',
+             desc: "An optional valid contact telephone number for the initiative",
+             comment: 'Historically, if not international, these have been UK phone numbers.',
             },
             {id: :email,
              header: 'Email',
-             desc: '',
+             desc: "An optional valid contact email address for the initiative",
              comment: '',
             },
             {id: :twitter,
              header: 'Twitter',
-             desc: '',
+             desc: "An optional valid URL for the initiative's main Twitter account",
              comment: '',
             },
             {id: :facebook,
              header: 'Facebook',
-             desc: '',
-             comment: '',
+             desc: <<-HERE,
+An optional valid path for the initiative's main Facebook presence. Should be
+prefixed with `http://facebook.com/` to get the actual URL.
+HERE
+             comment: <<-HERE
+Historically, this can also be an actual URL, but this can cause problems
+because of the prior assumption it is not.
+HERE
             },
             {id: :companies_house_number,
              header: 'Companies House Number',
-             desc: '',
+             desc: 'A UK Companies House Number, when applicable. Optional.',
              comment: '',
             },
             {id: :qualifiers,
              header: 'Qualifiers',
-             desc: '',
+             desc: <<-HERE,
+Additional qualifiers on the initiative. Allowed values are 
+zero or more term URIs from a relevant vocabulary, separated by semi-colons.
+HERE
              comment: '',
             },
             {id: :base_membership_type,
              header: 'Membership Type',
-             desc: '',
+             desc: <<-HERE,
+The membership type of the initiative. Allowed values are 
+zero or one term URIs from a relevant vocabulary.
+HERE
              comment: '',
             },
             {id: :latitude,
              header: 'Latitude',
-             desc: '',
+             desc: <<-HERE,
+An optional latitudinal coordinate of the initiative's main address, if known.
+A floating point number.
+HERE
              comment: '',
             },
             {id: :longitude,
              header: 'Longitude',
-             desc: '',
+             desc: <<-HERE,
+An optional longitudinal coordinage of the initiative's main address, if known.
+A floating point number.
+HERE
              comment: '',
             },
             {id: :geocontainer,
              header: 'Geo Container',
-             desc: '',
+             desc: <<-HERE,
+An optional identifier indicating a geographical region containing the initiative's
+main address, if known.
+HERE
              comment: '',
             },
             {id: :geocontainer_lat,
              header: 'Geo Container Latitude',
-             desc: '',
+             desc: <<-HERE,
+An optional latitudinal coordinate of the initiative's main address, when
+inferred from the address fields. A floating point number.
+HERE
              comment: '',
             },
             {id: :geocontainer_lon,
              header: 'Geo Container Longitude',
-             desc: '',
+             desc: <<-HERE,
+An optional longitudinal coordinate of the initiative's main address, when
+inferred from the address fields. A floating point number.
+HERE
              comment: '',
             },
           ]        
