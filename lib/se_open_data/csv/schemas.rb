e17a0a5e95c5a986aba3839b0a1a7f890f557414
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-03-02 14:31:50 +0000

    se_open_data/csv/schemas.rb - add Country ID and Territory ID fields to standard schema

--- lib/se_open_data/csv/schemas.rb
+++ lib/se_open_data/csv/schemas.rb
@@ -735,6 +735,185 @@ HERE
             },
           ]        
         ),
+        
+        SeOpenData::CSV::Schema.new(
+          id: :sse_initiatives,
+          name: "Solidarity Economy Initiatives",
+          version: 4,
+          description: <<-HERE,
+
+## Geographic coordinates
+
+`Latitude` and `Longitude` are for the exact geolocation of the SSE
+initiative, if it is known.
+
+If only a postcode or an address is known, use the `Geo Container *`
+fields.
+
+Often, we don't know the exact lat/long of an SSE initiative, but we
+know about a geographic container which has a known lat/long.  The
+most common example of this is the postcode as a geographic container.
+
+Note that the utility `bin/csv/standard/add-postcode-lat-long.rb` will
+populate these 3 fields based on the value of the `Postcode`, so don't
+do it manually!  This is used, for example, in the toolchain for
+generating the co-ops-uk 2017 RDF.
+
+## Sub-fields
+
+Sometimes a single column can take values that are in fact a list.  So
+we need to know the character used to separate the items in the list.
+For example, in the legal_form column, we might have an initiative
+that is both a 'Cooperative' and a 'Company', the cell would then have
+the value "Cooperative;Company"
+
+HERE
+          comment: 'Added "jurisdiction" field, redefined "country_name" as "country_id',
+          primary_key: [:id],
+          fields: [
+            {id: :id,
+             header: 'Identifier',
+             desc: 'A unique identifier for this initiative',
+             comment: <<-HERE
+As a consequence of needing to name generated static files with these 
+identifiers, which will then be published by a web server, they must be
+both a) valid as a portion of a file-name on the (typically, Unix) 
+host's file system, and b) as a URI segment as defined by RFC3986 
+<https://tools.ietf.org/html/rfc3986#section-3.3>
+HERE
+            },
+            {id: :name,
+             header: 'Name',
+             desc: '',
+             comment: '',
+            },
+            {id: :description,
+             header: 'Description',
+             desc: '',
+             comment: '',
+            },
+            {id: :organisational_structure,
+             header: 'Organisational Structure',
+             desc: '',
+             comment: <<-HERE,
+This was known as legal_form. The allowed values are taken from the
+relevant ESSGLOBAL SKOS vocabulary.
+HERE
+            },
+            {id: :primary_activity,
+             header: 'Primary Activity',
+             desc: '',
+             comment: '',
+            },
+            {id: :activities,
+             header: 'Activities',
+             desc: '',
+             comment: '',
+            },
+            {id: :street_address,
+             header: 'Street Address',
+             desc: '',
+             comment: '',
+            },
+            {id: :locality,
+             header: 'Locality',
+             desc: '',
+             comment: '',
+            },
+            {id: :region,
+             header: 'Region',
+             desc: '',
+             comment: '',
+            },
+            {id: :postcode,
+             header: 'Postcode',
+             desc: '',
+             comment: '',
+            },
+            {id: :country_id,
+             header: 'Country ID',
+             desc: <<-HERE,
+This indicates the country in which the main address is located.
+Should be an uppercase ISO-3166-1 two-letter country code.
+HERE
+             comment: '',
+            },
+            {id: :territory_id,
+             header: 'Territory ID',
+             desc: <<-HERE ,
+Indicates the territory in which the initiative operates. Values
+should be an ISO-3166-1 two-letter country code, or one of the
+ICA's region/super-region identifiers. Upper case where applicable.
+HERE
+             comment: 'There are ESSGLOBAL SKOS vocabs for the ISO and ICA identifiers',
+            },
+            {id: :homepage,
+             header: 'Website',
+             desc: '',
+             comment: '',
+            },
+            {id: :phone,
+             header: 'Phone',
+             desc: '',
+             comment: '',
+            },
+            {id: :email,
+             header: 'Email',
+             desc: '',
+             comment: '',
+            },
+            {id: :twitter,
+             header: 'Twitter',
+             desc: '',
+             comment: '',
+            },
+            {id: :facebook,
+             header: 'Facebook',
+             desc: '',
+             comment: '',
+            },
+            {id: :companies_house_number,
+             header: 'Companies House Number',
+             desc: '',
+             comment: '',
+            },
+            {id: :qualifiers,
+             header: 'Qualifiers',
+             desc: '',
+             comment: '',
+            },
+            {id: :base_membership_type,
+             header: 'Membership Type',
+             desc: '',
+             comment: '',
+            },
+            {id: :latitude,
+             header: 'Latitude',
+             desc: '',
+             comment: '',
+            },
+            {id: :longitude,
+             header: 'Longitude',
+             desc: '',
+             comment: '',
+            },
+            {id: :geocontainer,
+             header: 'Geo Container',
+             desc: '',
+             comment: '',
+            },
+            {id: :geocontainer_lat,
+             header: 'Geo Container Latitude',
+             desc: '',
+             comment: '',
+            },
+            {id: :geocontainer_lon,
+             header: 'Geo Container Longitude',
+             desc: '',
+             comment: '',
+            },
+          ]        
+        ),
       ]
       
       # Shortcut alias for the latest schema
