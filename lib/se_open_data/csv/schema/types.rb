18abcfd128837398660250ad33bcac3af56c77c2
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-04-22 17:27:24 +0100

    se_open_data/csv/schema/types.rb - adapt normalise_facebook to accept a list
    - optionally.  Retun the first successful case, or null

--- lib/se_open_data/csv/schema/types.rb
+++ lib/se_open_data/csv/schema/types.rb
@@ -33,26 +33,30 @@ module SeOpenData
           return default
         end
 
-        def self.normalise_facebook(str, base_url: 'https://www.facebook.com/')
-          return nil unless str
-          
-          url = normalise_url(str.to_s)
-          if url.nil? || url.empty?
-            Log.info "Ignoring un-normalisable URL: #{str}"
-            return nil
-          end
+        def self.normalise_facebook(items, base_url: 'https://www.facebook.com/')
+          return nil unless items
+          items = [items] unless items.respond_to? :each
 
-          # remove any query or anchor portion
-          url.sub!(/[?#].*/, '');
-          
-          url.downcase.match(%r{^https?://([\w_-]+\.)?facebook.com/?(.*)}) do |m|
-            return base_url+m[2]
+          items.each do |str|
+            Log.debug "Attempting to normalise FB URL: #{str}"
+            url = normalise_url(str.to_s)
+            if url.nil? || url.empty?
+              Log.info "Ignoring un-normalisable URL: #{str}"
+              next
+            end
+            
+            # remove any query or anchor portion
+            url.sub!(/[?#].*/, '');
+            
+            url.downcase.match(%r{^https?://([\w_-]+\.)?facebook.com/?(.*)}) do |m|
+              return base_url+m[2]
+            end
+            url.downcase.match(%r{^https?://([\w_-]+\.)?fb.me/?(.*)}) do |m|
+              return base_url+m[2]
+            end
+            
+            Log.info "Ignoring non-facebook URL: #{str}"
           end
-          url.downcase.match(%r{^https?://([\w_-]+\.)?fb.me/?(.*)}) do |m|
-            return base_url+m[2]
-          end
-
-          Log.info "Ignoring non-facebook URL: #{str}"
           return nil
         end
 
